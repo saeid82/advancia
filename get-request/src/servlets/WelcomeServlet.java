2abd5e0f85fa7ae56b69ca9642b7fd663034aa42
Saeid <Saeid@pc-casa>
2017-10-24 15:27:46 +0200

    esrcizio get request

---
+++ get-request/src/servlets/WelcomeServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String firstName = request.getParameter("firstname");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<?xml version=\"1.0\" encoding=\"${encoding}\" ?>");
+		out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "+
+		"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+		out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+		out.println("<head>");
+        out.println("<title> Processing get request with data </title>");
+        out.println("</head>");
+        
+        out.println("<body>");
+        out.println("<h1>");
+        out.println("Hello " +firstName + " !<br />");
+        out.println("</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+	}
+
+}
